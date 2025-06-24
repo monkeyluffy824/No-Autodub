c085869ec5f2e5d50e9d96224034878f9aa861a6
monkeyluffy824 <138200438+monkeyluffy824@users.noreply.github.com>
2025-06-24 23:23:08 +0530

    prototype

--- content.js
+++ content.js
@@ -1,66 +1,13 @@
-function determineType(){
-	let path=window.location.pathname;
-	let url=window.location.href;
-	if(path.startsWith('/shorts')){
-		return 'shorts';
-	}else if(path.startsWith('/watch') && url.includes('v=')){
-		return 'video';
-	}else{
-		return 'Other';
-	}
-	
-}
+const script = document.createElement("script");
+script.src = chrome.runtime.getURL("injected.js");
+script.onload = () => script.remove();
+(document.head || document.documentElement).appendChild(script);
 
-function checkIsVideoLoaded(){
-	let type= determineType();
-	if(type==='video'){
-		const player= document.getElementById('movie_player');
-		let videoData=player.getVideoData();
-		if(videoData!=null && videoData !=undefined){
-			let availableAudioTracks=player.getAvailableAudioTracks();
-			if(availableAudioTracks.length>1){
-				let currentAudioTrack=player.getAudioTrack();
-				let originalAudioTrack=availableAudioTracks.filter(track=>{
-					return track['H8']['name'].toLowerCase().includes('original')
-				});
-				if(originalAudioTrack.length==1){
-					if(currentAudioTrack['H8']['name']!=originalAudioTrack[0]['H8']['name']){
-						console.log('Shifting to original audio track');
-						if(player.setAudioTrack(originalAudioTrack[0])){
-							player.playVideo();
-						}
-					}
-				}
-			}
-		}else{
-			setTimeout(checkIsVideoLoaded(),100);
-		}
-	}else if(type==='shorts'){
-		const player= document.getElementById('shorts-player');
-		let videoData=player.getVideoData();
-		if(videoData!=null && videoData !=undefined){
-			let availableAudioTracks=player.getAvailableAudioTracks();
-			if(availableAudioTracks.length>1){
-				let currentAudioTrack=player.getAudioTrack();
-				let originalAudioTrack=availableAudioTracks.filter(track=>{
-					return track['H8']['name'].toLowerCase().includes('original')
-				});
-				if(originalAudioTrack.length==1){
-					if(currentAudioTrack['H8']['name']!=originalAudioTrack[0]['H8']['name']){
-						console.log('Shifting to original audio track');
-						if(player.setAudioTrack(originalAudioTrack[0])){
-							player.playVideo();
-						}
-					}
-				}
-				
-			}
-		}else{
-			setTimeout(checkIsVideoLoaded(),100);
-		}
-	}
-}
-
-checkIsVideoLoaded();
-
-			
+window.addEventListener("yt-navigate-finish", () => {
+  setTimeout(()=>{
+		const script = document.createElement("script");
+		script.src = chrome.runtime.getURL("injected.js");
+		script.onload = () => script.remove();
+		(document.head || document.documentElement).appendChild(script);
+  }, 500);
+});
